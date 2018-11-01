1b1f3d8b8eeb4de6c2660ad799d857ffea728777
Joey <joeyvaneijk@outlook.com>
2018-11-01 09:29:43 +0100

    Automatic type and multiplier assignment, throwing error when specified a wrong type.

--- game/classes/task.class.js
+++ game/classes/task.class.js
@@ -7,6 +7,7 @@ class Task extends Object{
         this._reward = reward;
         this._type = new TaskType(type);
         this._progress = 0;
+        this._types = [];
     }
     get name(){
         return this._name;
@@ -34,32 +35,59 @@ class Task extends Object{
 class TaskType extends Object{
     constructor(type){
         super();
-        this._type,this._multiplier = getTypeFromString(type);
-        
-        this._types = [
+        this._types = {
             // Powerpoint Dia's
-            ["Powerpoint_Learning"] = 4.0,
-            ["powerpoint"] = 2.0,
+            "Powerpoint_Learning" : 4.0,
+            "powerpoint" : 2.0,
             // Video Tapes
-            ["VideoTapes_Learning"] = 4.0,
-            ["videobanden"] = 2.0,
+            "VideoTapes_Learning" : 4.0,
+            "videobanden" : 2.0,
             // Photoshop
-            ["photoshop_Learning"] = 1.0,
-            ["photoshop"] = 1.2,
+            "photoshop_Learning" : 1.0,
+            "photoshop" : 1.2,
             // Coffee Mugs. Very important.
-            ["Mugs_Learning"] = 3.0,
-            ["coffee mugs"] = 1.5,
+            "Mugs_Learning" : 3.0,
+            "coffee mugs" : 1.5,
             // Video Editing
-            ["Video_Learning"] = 2.0,
-            ["Video"] = 1.0,
-        ]; 
+            "Video_Learning" : 2.0,
+            "Video" : 1.0
+        }; 
+        function validate(self,type){
+            var theMultiplier;
+            if(self._types[type] != null){
+                theMultiplier = self._types[type];
+                return type,theMultiplier
+            }
+            else{
+                return new Error("INVALID_TYPE");
+            }
+        };
+        this._type,this._multiplier = validate(this,type);
+        
+        
     }
+    get type(){
+        return this._type;
+    }
+    set type(newType){
+        this._type = newType;
+    }
+    get multiplier(){
+        return this._multiplier;
+    }
+    set multiplier(newMult){
+        this._multiplier = newMult;
+    }
+
+
     getTypeFromString(type){
         var theMultiplier;
-        if(this._types[type] != null){
-            theMultiplier = this._types[type];
+        if(this.types[type] != null){
+            theMultiplier = this.types[type];
             return type,theMultiplier
         }
-        return Error;
+        else{
+            return new Error("INVALID_TYPE");
+        }
     }
 }
