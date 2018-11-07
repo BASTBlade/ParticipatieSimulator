4d5cb0c52f446422cefd2c6d32a577bb19952cda
Joey <joeyvaneijk@outlook.com>
2018-11-07 11:43:38 +0100

    Edited tile class to support images and an absolete stat

--- game/maps/tile.class.js
+++ game/maps/tile.class.js
@@ -1,5 +1,21 @@
 class Tile extends Object{
-    constructor(){
+    constructor(image,absolete){
         super();
+        this._image = image;
+        this._absolete = absolete;
+    }
+
+    get image(){
+        return this._image;
+    }
+    set image(newImage){
+        this._image = newImage;
+    }
+
+    get absolete(){
+        return this._absolete;
+    }
+    set absolete(a){
+        this._absolete = a;
     }
 }
